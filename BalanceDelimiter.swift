b045b5ae59224f774efe14862acf39ec5e098c73
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-27 10:32:05 -0700

    Adding Balance Delimiter class

---
+++ BalanceDelimiter.swift
@@ -0,0 +1,14 @@
+//
+//  BalanceDelimiter.swift
+//  BalancedDelimiters
+//
+//  Created by Rodrigo Carballo on 3/27/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+class BalanceDelimiter
+{
+    
+}
